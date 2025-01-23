788ad5c0c7965dc0b017b988be9feddd4b80fe62
Kaique Cavalcante <kaiquesantos190@gmail.com>
2025-01-22 22:52:16 -0300

    Add fzf logic with merge and switch branch functions

--- auto-git.sh
+++ auto-git.sh
@@ -1,8 +1,51 @@
 #!/bin/bash
 
-selected=$(git branch | fzf --height 40% --layout reverse --border)
+# Função para tratamento de erro
+function exit_exception() {
+    if [ $? -eq 130 ]; then
+echo "Exiting.."
+exit 1
+fi
+}
+
+# Função para switch de branch
+function switch_branch() {
+    selected=$(git branch | fzf +m \
+    --height 40% \
+    --layout reverse \
+    --border  \
+    --preview \
+            'git -c color.ui=always log --oneline $(echo {} | tr -d "* ")' \
+    --color bg:#222222,preview-bg:#333333)
+
 selected=$(echo $selected | tr -d "* ")
+
+exit_excepiotn
 
 echo ">>>> $selected"
 
-git switch $selected
+git switch "$selected"
+
+}
+
+# Função de Merge
+function merge() {
+    selected=$(git branch | fzf +m \
+    --height 100% \
+    --layout reverse \
+    --border  \
+    --preview \
+            'git -c color.ui=always diff $(git branch | grep "^*" | tr -d "* " ) $(echo {} | tr -d "* ")' \
+    --color bg:#222222,preview-bg:#333333)
+
+selected=$(echo $selected | tr -d "* ")
+
+exit_exception
+
+echo ">>>> $selected"
+
+git merge "$selected"
+}
+
+merge
+
